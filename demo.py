6af6fcf1e5bba0d8ac205bc409c5eb7989293d92
Mark OBrien <mark_noreply@noreply.com>
2020-02-16 20:47:34 -0500

    Remote queue drives the video

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import redis
 import tensorflow as tf
-
+import time
 from utilities import from_redis
 
 
@@ -22,16 +22,46 @@ def main():
     # mean_cube = np.load('models/train01_16_128_171_mean.npy')
     # mean_cube = np.transpose(mean_cube, (1, 2, 3, 0))
     # read_frames = r.xread({'vidstream': b'1-0'})
-    # read_inference = r.xread({'infstream': b'0-0'})  # number of messages and block count in ms
     # read_frames = r.xrange('vidstream', '-', '+', 5)
-    read_inference = r.xrange('infstream', '-', '+', 5)  # number of messages and block count in ms
+    # read_inference = r.xrange('infstream', '-', '+', 5)  # number of messages and block count in ms
+
+    read_inference = r.xread({'infstream': b'0-0'})  # number of messages and block count in ms
 
 
-    vid = []
-    for k, item in enumerate(read_inference):
-        timestamp_idx = item[1][b'timestamp']
-        p_label = item[1][b'prediction']
-        print(p_label)
+    VIDEO_FILE = './videos/tennis.mp4'
+    video_file = VIDEO_FILE
+    cap = cv2.VideoCapture(video_file)
+    if not cap:
+        print("No video loaded {}".format(video_file))
+        exit()
+    idx = 0
+    while True:
+        ret, img = cap.read()
+        if not ret:  # end of stream
+            break
+
+        # img = np.array([img], dtype=np.float32)  # TODO retain uint8 and convert to float32 on the other end to reduce transport cost
+        img = img / 255
+
+        sframe = read_inference[0][1][idx]
+
+        idx += 1
+        p_label = sframe[1][b'prediction'].decode('utf-8')
+
+        cv2.putText(img, p_label, (11, 21), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 0), 1)
+        cv2.putText(img, p_label, (10, 20), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 100, 0), 1)
+
+        cv2.imshow('Sporty', img)
+        time.sleep(.05)
+        # cv2.moveWindow('Sporty', 300, 150)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # for k, item in enumerate(read_inference[0][1]):
+    #     timestamp_idx = item[1][b'timestamp'].decode('utf-8')
+    #     p_label = item[1][b'prediction'].decode('utf-8')
+    #     print(p_label)
+
         # img = from_redis(item[1][b'frame'])
         # timestamp_idx = item[0].decode('utf-8')
         #
